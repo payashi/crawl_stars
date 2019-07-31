da2b97feaa065a8d59ebd49e7e420547870d04a6
hiromichi hayashi <hayashi@mba.local>
2019-07-31 17:38:47 +0900

    create hayashi ai

--- res/main.py
+++ res/main.py
@@ -7,17 +7,17 @@ from comp import mv
 def main():
     st = lib.SimpleStage()
     st.register_players(
-        lib.Player((256, 0, 0), "Sakaguchi", 0),
-        lib.Player((0, 0, 256), "Sakaguchi", 1)
+        lib.Player((256, 0, 0), "Hayashi", 0),
+        lib.Player((0, 0, 256), "Yamatope", 1)
     )
     while(st.frame<=stg.MAX_FRAME):
         sys.stdout.write("\r{:0=3} / {} frame".format(st.frame, stg.MAX_FRAME))
         sys.stdout.flush()
-        time.sleep(0.01)
+        # time.sleep(0.01)
         st.pre_draw()
         lib.Character.all_passive_change(st)
-        mv.hayashi_moves(st.players[0])
-        mv.yamatope_moves(st.players[1])
+        mv.lethal_hayashi(st.players[0])
+        mv.weak_hayashi(st.players[1])
         lib.Character.all_move(st)
         lib.Bullet.all_move(st)
         lib.Character.all_lethal_blow(st)
